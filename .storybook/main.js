77e4ac6a1a1f2bff5930eb86571b7f2794aab5d9
Yakov Rakhamimov <jacobra19@gmail.com>
2020-08-10 23:46:55 +0300

    storybook webpack from app config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,12 @@
+const path = require('path');
+
+// your app's webpack.config.js
+const custom = require('../webpack.config');
+
+
 module.exports = {
     stories: ['../src/**/*.stories.[tj]s'],
+    webpackFinal: (config) => {
+        return { ...config, module: { ...config.module, rules: custom.module.rules } };
+      },
 };
